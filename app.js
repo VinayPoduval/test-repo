509d78ebe356159bb55eca59f8cb6622b7521cf4
Vinay Poduval <vinaypoduval@hotmail.com>
2021-02-12 17:04:51 +0530

    Fixed first request bug

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 //import axios from 'axios';
 
 const form = document.querySelector('#form');
-const formEvent = form.addEventListener('submit', function() {
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const contact = document.querySelector('#phone').value;
+const formEvent = form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const contact = document.querySelector('#phone');
 
     const person = {
-        name,
-        email,
-        contact
+        name: name.value,
+        email: email.value,
+        contact: contact.value
     };
 
     try {
-        axios.post('https://hooks.zapier.com/hooks/catch/9484398/opr5hu8/', person, {
+        const res = await axios.post('https://hooks.zapier.com/hooks/catch/9484398/opr5hu8/', person, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             }
         });
+
+        if(res.status === 200) {
+            name.value = '';
+            email.value = '';
+            contact.value = '';
+        }
     }
     catch(e) {
         console.error('e')
