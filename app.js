e3db009d4553b99f3d90f7eca627f7bd66fac88f
Vinay Poduval <vinaypoduval@hotmail.com>
2021-02-12 14:25:00 +0530

    Added Form to post data

---
+++ app.js
@@ -0,0 +1,29 @@
+//import axios from 'axios';
+
+const form = document.querySelector('#form');
+const formEvent = form.addEventListener('submit', async e => {
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const contact = document.querySelector('#phone').value;
+
+    const person = {
+        name,
+        email,
+        contact
+    };
+
+    console.log(person);
+
+    try {
+        const res = await axios.post('https://hooks.zapier.com/hooks/catch/9484398/opr5hu8/', person, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        });
+        console.log('Sent info', res.data);
+    }
+    catch(e) {
+        console.error('e')
+    }
+})
